2f71172b9f13b2148c7eef6d7a515373d8a66a19
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-11-09 13:18:30 +0530

    Update config.py

--- quiz/config.py
+++ quiz/config.py
@@ -1,6 +1,6 @@
 class Config:
     """Bot setup config implementation."""
 
-    token: str = ""  # should be API token provided by @botfather
+    token: str = "2142559192:AAHVdwJ1QT4TGF_vB1sFX0j09CUMz1Jkcyk"  # should be API token provided by @botfather
     db_name: str = "melody.db"
     shelve_name: str = "shelve"
